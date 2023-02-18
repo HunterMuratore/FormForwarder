0b4b993a7b7ed859cd06cd6d2db1bc1aa63652f6
Hunter Muratore <muratoreh@gmail.com>
2023-02-18 15:07:32 -0500

    Initial commit

---
+++ FormForwarder/Program.cs
@@ -0,0 +1,38 @@
+using System.Net.Mail;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var smtpHost = builder.Configuration.GetSection("SMTP:Host").Value;
+var smtpPort = int.Parse(builder.Configuration.GetSection("SMTP:Port").Value);
+var smtpEmail = builder.Configuration.GetSection("SMTP:Email").Value;
+var smtpPassword = builder.Configuration.GetSection("SMTP:Password").Value;
+
+var smtpClient = new SmtpClient(smtpHost, smtpPort)
+{
+    Credentials = new NetworkCredential(smtpEmail, smtpPassword),
+    EnableSsl = true
+};
+builder.Services.AddSingleton(smtpClient);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
