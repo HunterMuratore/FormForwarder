c66e3913b26804e07a86a49fbc238d30a22c6a56
Hunter Muratore <muratoreh@gmail.com>
2023-02-27 17:15:05 -0500

    added email validation and PORT variable to .env-template

--- FormForwarder/Controllers/FormForwarderController.cs
+++ FormForwarder/Controllers/FormForwarderController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mail;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace FormForwarder.Controllers
 {
@@ -8,6 +9,7 @@ namespace FormForwarder.Controllers
     [Route("")]
     public class FormForwarderController : ControllerBase
     {
+        private const string EMAIL_VALIDATE_REGEX = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
         private const string EMAIL_SUBJECT = "Form Submitted";
 
         private readonly ILogger<FormForwarderController> _logger;
@@ -29,47 +31,50 @@ namespace FormForwarder.Controllers
 
             if (!string.IsNullOrEmpty(email))
             {
-                foreach (var key in HttpContext.Request.Form.Keys)
-                {
-                    var val = HttpContext.Request.Form[key];
-                    if (key == "subject")
-                    {
-                        emailSubject = val;
-                    }
-                    else if (string.IsNullOrEmpty(val.ToString()))
-                    {
-                        builder.AppendLine("The user did not provide a " + key);
-                        builder.AppendLine();
-                    }
-                    else
-                    {
-                        builder.AppendLine(key + ":");
-                        builder.AppendLine();
-                        builder.AppendLine(val.ToString());
-                        builder.AppendLine();
-                    }
-                }
-
-                if (emailSubject == string.Empty)
-                {
-                    emailSubject = EMAIL_SUBJECT;
-                }
-
-                try
-                {
-                    _smtpClient.Send(_email, email, emailSubject, builder.ToString());
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex.ToString());
-                    return Problem();
-                }
-                return Ok();
+                return BadRequest("Must enter an email address.");
             }
-            else
+
+            var emailMatch = Regex.Match(email, EMAIL_VALIDATE_REGEX, RegexOptions.IgnoreCase);
+            if (!emailMatch.Success) {
+                return BadRequest("Must enter an email address in valid email address form.");
+            }
+
+            foreach (var key in HttpContext.Request.Form.Keys)
             {
-                return BadRequest();
+                var val = HttpContext.Request.Form[key];
+                if (key == "subject")
+                {
+                    emailSubject = val;
+                }
+                else if (string.IsNullOrEmpty(val.ToString()))
+                {
+                    builder.AppendLine("The user did not provide a " + key);
+                    builder.AppendLine();
+                }
+                else
+                {
+                    builder.AppendLine(key + ":");
+                    builder.AppendLine();
+                    builder.AppendLine(val.ToString());
+                    builder.AppendLine();
+                }
             }
+
+            if (emailSubject == string.Empty)
+            {
+                emailSubject = EMAIL_SUBJECT;
+            }
+            try
+            {
+                _smtpClient.Send(_email, email, emailSubject, builder.ToString());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                return Problem();
+            }
+
+            return Ok();
         }
     }
 }
