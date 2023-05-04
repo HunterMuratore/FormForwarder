afb702ce39f7b695a899d74f6b18aab7e6dc0ec5
Hunter Muratore <muratoreh@gmail.com>
2023-05-04 13:12:23 -0400

    Email validation tests

--- FormForwarder Tests/FormForwarderTests.cs
+++ FormForwarder Tests/FormForwarderTests.cs
@@ -1,16 +1,39 @@
+using FormForwarder.Controllers;
+
 namespace FormForwarder_Tests
 {
-    public class Tests
+    public class FormForwarderTests
     {
+        private FormForwarderController _postRequest;
+
         [SetUp]
         public void Setup()
         {
+            _postRequest = new FormForwarderController();
         }
 
         [Test]
-        public void Test1()
+        public async void Post_NullEmail_ReturnsBadRequestEmailNull()
         {
-            Assert.Pass();
+            var email = "";
+
+            var response = _postRequest(email);
+
+            Assert.IsInstanceOf<BadRequestErrorMessageResult>(response);
+            Assert.AreEqual("Must enter an email address.", response.Message);
+        }
+
+        [TestCase("huntergmail.com")]
+        [TestCase("hunter@gmailcom")]
+        [TestCase("@gmail.com")]
+        [TestCase("hunter@")]
+        [TestCase("hunter@.com")]
+        public async void Post_InvalidEmail_ReturnsBadRequestEmailInvalid(string email)
+        {
+            var response = _postRequest(email);
+
+            Assert.IsInstanceOf<BadRequestErrorMessageResult>(response);
+            Assert.AreEqual("Must enter an email address in valid email address form (your@email.com).", response.Message);
         }
     }
 }
